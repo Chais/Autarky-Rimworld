8270360878286659da54a5a8b51f7679c0d2078e
Philip Abernethy <chais.z3r0@gmail.com>
2017-07-01 01:45:50 +0200

    Release v1.0

--- README.md
+++ README.md
@@ -11,5 +11,5 @@ extract Devil's Dust from Devilstrand. I'll leave the rest for you to discover.
 
 # Acknowledgement
 This is my first mod for RimWorld and it was created in about a week, not least thanks to the
-kind people in [#rimworld-modding](https://discord.gg/0kEwljX6FiCur2uN). Especially I want to thank erdelf, NotFood, Mehni, ChJees, spdskatr, Swenzi, Xen, Why_is_that, Alistaire and probably other whom I forgot. So thank you. Without this wouldn't have been done in so little time.
+kind people in [#rimworld-modding](https://discord.gg/0kEwljX6FiCur2uN). Especially I want to thank erdelf, NotFood, Mehni, ChJees, spdskatr, Swenzi, Xen, Why_is_that, Alistaire and probably other whom I forgot. So thank you. Without this wouldn't have been done in so little time.  
 And also thank you to Marnador for his RimWorld Font.
---
+++ Source/CompProperties_Explosive.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+namespace Autarky
+{
+	public class CompProperties_Explosive : CompProperties
+	{
+		//
+		// Fields
+		//
+		public float explosiveRadius = 1.9f;
+
+		public float explosiveExpandPerStackcount;
+
+		public EffecterDef explosionEffect;
+
+		public DamageDef startWickOnDamageTaken;
+
+		public float startWickHitPointsPercent = 0.2f;
+
+		public IntRange wickTicks = new IntRange (140, 150);
+
+		public float wickScale = 1f;
+
+		public float chanceNeverExplodeFromDamage;
+
+		public int preExplosionSpawnThingCount = 1;
+
+		public DamageDef explosiveDamageType = DamageDefOf.Bomb;
+
+		public ThingDef postExplosionSpawnThingDef;
+
+		public float postExplosionSpawnChance;
+
+		public int postExplosionSpawnThingCount = 1;
+
+		public bool applyDamageToExplosionCellsNeighbors;
+
+		public ThingDef preExplosionSpawnThingDef;
+
+		public float preExplosionSpawnChance;
+
+		//
+		// Constructors
+		//
+		public CompProperties_Explosive ()
+		{
+			this.compClass = typeof(CompExplosive);
+		}
+	}
+}
